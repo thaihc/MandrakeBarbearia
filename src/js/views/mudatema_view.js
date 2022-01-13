5f242def39c2545091fc89c58fa7522489778f7f
anaabalbi <anaabalbi@gmail.com>
2022-01-13 15:48:10 -0300

    mudando a raiz das imagens

--- src/js/views/mudatema_view.js
+++ src/js/views/mudatema_view.js
@@ -1,22 +1,10 @@
 class ViewTema {
   mudaTema(tema) {
-    $('.tema').attr('src', `/MandrakeBarbearia/src/img/modo-${tema}.png`);
-    $('.imgLogo').attr('src', `/MandrakeBarbearia/src/img/logo-${tema}.png`);
-    $('.localizacao').attr(
-      'src',
-      `/MandrakeBarbearia/src/img/localizacao-${tema}.png`
-    );
-    $('.telefone').attr(
-      'src',
-      `/MandrakeBarbearia/src/img/icon-telefone-${tema}.png`
-    );
-    $('.whatsapp').attr(
-      'src',
-      `/MandrakeBarbearia/src/img/icon-wpp-${tema}.png`
-    );
-    $('.mensagem').attr(
-      'src',
-      `/MandrakeBarbearia/src/img/icon-mensagem-${tema}.png`
-    );
+    $('.tema').attr('src', `/src/img/modo-${tema}.png`);
+    $('.imgLogo').attr('src', `/src/img/logo-${tema}.png`);
+    $('.localizacao').attr('src', `/src/img/localizacao-${tema}.png`);
+    $('.telefone').attr('src', `/src/img/icon-telefone-${tema}.png`);
+    $('.whatsapp').attr('src', `/src/img/icon-wpp-${tema}.png`);
+    $('.mensagem').attr('src', `/src/img/icon-mensagem-${tema}.png`);
   }
 }
