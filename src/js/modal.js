6e62f703e81843d0b8baa643688b5edb3d15be3c
thaihc <thaisherculanodecarvalho@gmail.com>
2022-01-14 16:06:48 -0300

    Adicionei o modal de contato

---
+++ src/js/modal.js
@@ -0,0 +1,14 @@
+function startModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    modal.classList.add("mostrar");
+    modal.addEventListener("click", (e) => {
+      if (e.target.id == modalId || e.target.className == "fechar") {
+        modal.classList.remove("mostrar");
+      }
+    });
+  }
+}
+
+const mensagem = document.getElementById("iconFooter3");
+mensagem.addEventListener("click", () => startModal("modalContato"));
