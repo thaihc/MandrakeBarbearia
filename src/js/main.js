04196f4549fe1d9af2e43a6093e07c531010e674
anaabalbi <anaabalbi@gmail.com>
2022-01-14 15:18:21 -0300

    arquivo main.js eventos de click para o carrossel

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
 $('.tema').click((e) => {
-  console.log('aiai');
   const controller = new ControllerTema();
   controller.checaTema();
+});
+
+$(document).ready((e) => {
+  let contador = 0;
+  $('.prev').click((e) => {
+    const slides = document.querySelector('.slide').children;
+    const controller = new SlideControllerPrev(slides.length, contador);
+    controller.qualSlide();
+    contador = controller.contador;
+    console.log(controller.contador);
+  });
+});
+
+$(document).ready((e) => {
+  let contador = 0;
+  $('.next').click((e) => {
+    const slides = document.querySelector('.slide').children;
+    console.log(slides);
+    const controller = new SlideController(slides.length, contador);
+    controller.qualSlide();
+    contador = controller.contador;
+    console.log(controller.contador);
+  });
 });
