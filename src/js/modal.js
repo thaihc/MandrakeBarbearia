c4108e8bfef09da295550371328267e5deed6e97
anaabalbi <anaabalbi@gmail.com>
2022-01-16 16:47:15 -0300

    refatorando e criando o evento de click para a class mensagem

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,20 @@
 function startModal(modalId) {
   const modal = document.getElementById(modalId);
   if (modal) {
-    modal.classList.add("mostrar");
-    modal.addEventListener("click", (e) => {
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
       if (
         e.target.id == modalId ||
-        e.target.className == "fechar" ||
-        e.target.className == "botao"
+        e.target.className == 'fechar' ||
+        e.target.className == 'botao'
       ) {
-        modal.classList.remove("mostrar");
+        modal.classList.remove('mostrar');
       }
     });
   }
 }
 
-const mensagem = document.getElementById("iconFooter3");
-mensagem.addEventListener("click", () => startModal("modalContato"));
+const mensagem = document.getElementById('iconFooter3');
+mensagem.addEventListener('click', () => startModal('modalContato'));
+const mensagem1 = document.querySelector('.mensagem');
+mensagem1.addEventListener('click', () => startModal('modalContato'));
