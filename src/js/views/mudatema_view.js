67ad28a8a05657f8fd7f8ed4147e299a17109184
anaabalbi <anaabalbi@gmail.com>
2022-01-17 13:03:38 -0300

    refatoramento class btn-sandwich0 e btn-sandwich1

--- src/js/views/mudatema_view.js
+++ src/js/views/mudatema_view.js
@@ -6,7 +6,8 @@ class ViewTema {
     $('.telefone').attr('src', `/src/img/icon-telefone-${tema}.png`);
     $('.whatsapp').attr('src', `/src/img/icon-wpp-${tema}.png`);
     $('.mensagem').attr('src', `/src/img/icon-mensagem-${tema}.png`);
-    $('.menu').attr('src', `/src/img/menu-${tema}.png`);
+    $('.btn-sandwich0').attr('src', `/src/img/menu-${tema}.png`);
+    $('.btn-sandwich1').attr('src', `/src/img/menu-${tema}.png`);
     $('#iconFooter1').attr('src', `/src/img/icons/telefone-${tema}.png`);
     $('#iconFooter2').attr('src', `/src/img/icons/wpp-${tema}.png`);
     $('#iconFooter3').attr('src', `/src/img/icons/email-${tema}.png`);
