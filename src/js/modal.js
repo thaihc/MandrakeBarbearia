0e551799048ada1b57fb7934261c6b54cc302ca1
anaabalbi <anaabalbi@gmail.com>
2022-01-25 16:29:52 -0300

    inciando os eventos de modal nos botões mais informações

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,6 @@
 function startModal(modalId) {
   const modal = document.getElementById(modalId);
+  console.log(modal);
   if (modal) {
     modal.classList.add('mostrar');
     modal.addEventListener('click', (e) => {
@@ -16,5 +17,7 @@ function startModal(modalId) {
 
 const mensagem = document.getElementById('iconFooter3');
 mensagem.addEventListener('click', () => startModal('modalContato'));
-const mensagem1 = document.querySelector('.mensagem');
-mensagem1.addEventListener('click', () => startModal('modalContato'));
+const maisInfo = document.querySelector('.btn');
+maisInfo.addEventListener('click', () => startModal('modalContato'));
+const maisInfo1 = document.querySelector('.btn1');
+maisInfo1.addEventListener('click', () => startModal('modalContato'));
