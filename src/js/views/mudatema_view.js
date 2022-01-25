b2a8dde812c474783cfec28cd50cbc575d66e204
anaabalbi <anaabalbi@gmail.com>
2022-01-25 15:56:24 -0300

    inserindo as classe das imagens da página produtos.html no mudatema_view

--- src/js/views/mudatema_view.js
+++ src/js/views/mudatema_view.js
@@ -12,5 +12,17 @@ class ViewTema {
     $('#iconFooter2').attr('src', `/src/img/icons/wpp-${tema}.png`);
     $('#iconFooter3').attr('src', `/src/img/icons/email-${tema}.png`);
     $('.who').attr('src', `/src/img/who-${tema}.png`);
+    $('.cardImg1').attr('src', `/src/img/s1-${tema}.png`);
+    $('.cardImg2').attr('src', `/src/img/s2-${tema}.png`);
+    $('.cardImg3').attr('src', `/src/img/s3-${tema}.png`);
+    $('.cardImg4').attr('src', `/src/img/s4-${tema}.png`);
+    $('.cardImg5').attr('src', `/src/img/s5-${tema}.png`);
+    $('.cardImg6').attr('src', `/src/img/s6-${tema}.png`);
+    $('.cardImg7').attr('src', `/src/img/p1-${tema}.png`);
+    $('.cardImg8').attr('src', `/src/img/p2-${tema}.png`);
+    $('.cardImg9').attr('src', `/src/img/p3-${tema}.png`);
+    $('.cardImg10').attr('src', `/src/img/t1-${tema}.png`);
+    $('.cardImg11').attr('src', `/src/img/t2-${tema}.png`);
+    $('.cardImg12').attr('src', `/src/img/t3-${tema}.png`);
   }
 }
